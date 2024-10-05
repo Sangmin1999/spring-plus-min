24ddd6a425689c2782f0810167b4eba029382cb9
Sangmin Lee <sart5534@gmail.com>
2024-10-05 19:49:12 +0900

    9. Spring  Security

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -2,19 +2,35 @@ package org.example.expert.domain.common.dto;
 
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 public class AuthUser {
 
     private final Long id;
     private final String email;
-    private final UserRole userRole;
+    private final Collection<? extends GrantedAuthority> authorities;
     private final String nickname;
 
-    public AuthUser(Long id, String email, UserRole userRole, String nickname) {
+    public AuthUser(Long id, String email, UserRole role, String nickname) {
         this.id = id;
         this.email = email;
-        this.userRole = userRole;
+        this.authorities = List.of(new SimpleGrantedAuthority(role.name()));
         this.nickname = nickname;
+
+    }
+
+    /**
+     * authorities에서 첫 번째 권한(GrantedAuthority)을 가져옵니다.
+     * 그 권한의 이름을 getAuthority() 메서드를 사용해 문자열로 추출합니다.
+     * 그 권한 이름을 기반으로 UserRole.of()를 호출하여 해당 문자열을 UserRole Enum 값으로 변환합니다.
+     * @return
+     */
+    public UserRole getRole() {
+        return UserRole.of(authorities.iterator().next().getAuthority());
     }
 }
